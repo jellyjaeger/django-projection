2b725141fc95c1eaa7d0ebd20ac30a1ce1b17e44
SOORAJ TS <sjts007@gmail.com>
2020-09-20 22:49:28 +0530

    added the requirement and gitignore

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -130,5 +130,5 @@ EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS =True
 EMAIL_HOST_USER = 'campuschats200@gmail.com'
-EMAIL_HOST_PASSWORD = '********'
+EMAIL_HOST_PASSWORD = 'Soorajsivadas767'
 #I cant give my password here!
